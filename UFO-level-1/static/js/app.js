97409e32ed67a203176ffaa3ff248a29955cd871
Althea McMillian <amcmilli@richmond.edu>
2020-08-26 22:41:43 -0400

    displaying data but not within columns

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,4 +5,15 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Console.log the data from data.js
-console.log(data);
+console.log(tableData);
+
+//
+data.forEach(function(sightingReport) {
+    console.log(sightingReport);
+    var row = tbody.append("tr");
+     Object.entries(sightingReport).forEach(function([key, value]) {
+       console.log(key, value);
+       var cell = tbody.append("td");
+       cell.text(value);
+     });
+   });
