bbc49f09ffb3cc3a2e73ade20bd2847a72ae915b
Althea McMillian <amcmilli@richmond.edu>
2020-08-26 22:12:54 -0400

    referencing the table and and displaying data into console

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,8 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the data from data.js
+console.log(data);
